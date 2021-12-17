207002ab78f4077a8ec8346c1df1d4129bf708ba
Kenji Saito <1867845+poad@users.noreply.github.com>
2021-12-17 09:04:24 +0900

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <Component {...pageProps} />
+);
+
+export default App;
