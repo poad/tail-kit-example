e84f9511ca0f14d18f5dc82daee5704a7c0a870d
Kenji Saito <ken-yo@mbr.nifty.com>
2022-03-15 09:36:35 +0900

    Fix error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
