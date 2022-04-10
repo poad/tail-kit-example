694ee4372f4ddbf159af20bf78f26bc828607964
Kenji Saito <ken-yo@mbr.nifty.com>
2022-04-11 08:24:28 +0900

    Fix the build error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Component {...pageProps} />
+const App = ({ Component }: AppProps): JSX.Element => (
+  <Component />
 );
 
 export default App;
